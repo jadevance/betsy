42825167ff3431d8c688c1fc49c0586734c5d37e
Mindy Carson <mcarson1111@gmail.com>
2016-05-25 15:35:49 -0700

    fixed the session crap

--- app/controllers/Carriers_controller.rb
+++ app/controllers/Carriers_controller.rb
@@ -3,7 +3,11 @@ require 'CarriersWrapper'
 class CarriersController
 
   def create
-    @destination = (params["city"], params["state"], params["zip"])
+    @city = params[:order][:city]
+    @state = params[:order][:state]
+    @zip = params[:order][:zip]
+
+    @destination = (@city, @state, @zip)
     @destination_query = CarriersWrapper.fetch(@destination)
     # this gets sent to the wrapper
 
@@ -12,6 +16,12 @@ class CarriersController
   end
 
 
+  def order_items
+    #need to go through the whole order and find the weight of each object....
+    #create a hash of each item
+    #order item info array of hashes...
+
+  end
 
 
 
